87f44eb7b6c04358d0c251bd23058046c934d9c6
ZiPaXG <shepelev.danilka@bk.ru>
2023-11-17 20:07:06 +0300

    добавление координат карт

--- views/card_view.py
+++ views/card_view.py
@@ -7,6 +7,11 @@ class CardView:
         self.width = GEOMETRY["card"][0]
         self.height = GEOMETRY["card"][1]
         self.img = pygame.transform.scale(pygame.image.load(path_img), (self.width, self.height))
+        self.first_point = [0, 0]
+        self.second_point = [0, 0]
 
     def draw(self, display: pygame.Surface, x: int, y: int):
+        self.first_point = [x, y]
+        self.second_point = [x + self.width, y + self.height]
         display.blit(self.img, (x, y))
+        print(self.first_point, self.second_point)
