4604736437ff893c5c799e1195d32a41f36aab83
ZiPaXG <shepelev.danilka@bk.ru>
2023-11-18 23:07:37 +0300

    альфа-версия игры

--- views/application.py
+++ views/application.py
@@ -42,9 +42,11 @@ class Application:
                                 self.vgame.rule_cards_view.pop(1)
                                 self.deck.remove_rule_card_from_deck()
                             else:
-                                for i in range(len(self.vgame.players_cards_view[0])):
-                                    if self.vgame.players_cards_view[0][i].rectangle.collidepoint(pygame.mouse.get_pos()):
-                                        Game.check_selected_card(list_players[0], self.deck.get_rule_deck()[1], self.deck.get_players_deck()[0], i)
+                                for i in range(len(self.vgame.players_cards_view[1])):
+                                    if self.vgame.players_cards_view[1][i].rectangle.collidepoint(pygame.mouse.get_pos()):
+                                        Game.check_selected_card(list_players[0], self.deck.get_rule_deck()[1], self.deck.get_players_deck()[1], i)
+                                        self.vgame.rule_cards_view.pop(1)
+                                        self.deck.remove_rule_card_from_deck()
 
                 for i in self.vgame.players_cards_view[0]:
                     i.check_collide_rect(self.display)
